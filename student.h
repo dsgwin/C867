089965d6f7bd44b1d2c454935fd71485dfb8aa28
dsgwin <77014340+dsgwin@users.noreply.github.com>
2023-03-22 22:35:59 -0400

    Update student.h

--- student.h
+++ student.h
@@ -10,11 +10,23 @@ public:
     Student();
 
     // Mutator Functions to Set Private Variables
+    void SetStudenId(string studentId);
     void SetFirstName(string firstName);
+    void SetLastName(string lastName);
+    void SetEmailAddress(string emailAddress);
+    void SetAge(int age);
+    void SetDaysToComplete(int course, int days);
+    void SetDegreeProgram(enum degreeProgram);
 
     // Accessor Functions to Get Private Variables
 
     string GetFirstName();
+    string GetFirstName();
+    string GetLastName();
+    string GetEmailAddress();
+    int GetAge();
+    int GetDaysToComplete();
+    enum GetDegreeProgram();
 
 private:
     string studentId;
